436d5d38c3c39b4498526585bac11a799d3e1bb9
AngeloDamante <angelo.damante16@gmail.com>
2023-05-05 12:44:48 +0200

    feat: add make_segmaps_with_spheres function for technique 1 and clean code

--- preprocessing/create_segmaps.py
+++ preprocessing/create_segmaps.py
@@ -1,74 +1,71 @@
-import random
+import os
 from PIL import Image
-import xml.etree.ElementTree as ET
 import torch
-from cv2 import cv2
+from torchvision.utils import save_image
+import torch.nn.functional as F
 from preprocessing.analyser import PATH_GTH, PATH_IMG
-from preprocessing.analyser import extract_particles, query_particles
+from preprocessing.analyser import extract_particles, query_particles, get_img_path, get_gth_path
 from preprocessing.loader import get_slice
 from preprocessing.Particle import Particle, SNR, Density
 import numpy as np
 import matplotlib.pyplot as plt
-import torch.nn.functional as F
+from tqdm import tqdm
+
+PATH_DTS_1 = "../Dataset/seg_technique_1"
+SIZE_VOL = (512, 512, 10)
 
 
-# blank image
-size = (512, 512, 10)  # aumentare il numero di slice
-seg_map = torch.zeros(size=size, dtype=torch.uint8)
+# technique 1
+def make_segmaps_with_spheres(snr: SNR, density: Density, radius: int = 1, value: int = 1) -> bool:
+    """Segmentation map maker
 
-# t0
-particles = extract_particles(f'{PATH_GTH}/VIRUS_snr_7_density_low.xml')
-particles_t0 = query_particles(particles, (lambda pa: True if pa.t == 0 else False))
+    :param snr:
+    :param density:
+    :param radius:
+    :param value:
+    :return:
+    """
+    # take particle
+    path_gth = get_gth_path(snr, density)
+    particles = extract_particles(path_gth)
+    if len(particles) == 0: return False
 
-# put disks
-for p in particles_t0:
-    seg_map[round(p.x), round(p.y), np.clip(round(p.z), 0, 9)] = 255
+    # make 3d images for each t
+    for time in tqdm(range(100)):
+        # get particles at time t
+        particles_t = query_particles(particles, (lambda pa, time=time: True if pa.t == time else False))
 
-# p_rand = random.choice(particles_t0)
-# print(seg_map[round(p_rand.x), round(p_rand.y), round(p_rand.z)].item())
+        # make blank 3d image
+        img_3d = np.zeros(shape=SIZE_VOL)
+        x, y, z = np.meshgrid(np.arange(img_3d.shape[0]), np.arange(img_3d.shape[1]), np.arange(img_3d.shape[2]))
 
-# take slices at t=0
-slices_t0 = []
-for depth in range(10):
-    slices_t0.append(np.asarray(get_slice(SNR.TYPE_7, Density.LOW, t=0, depth=depth)))
-img_3d = np.stack(slices_t0, axis=2)
-print(img_3d.shape)
+        # draw spheres centered in known particles
+        for p in particles_t:
+            center = (round(p.x), round(p.y), np.clip(round(p.z), 0, 9))
+            distance = np.sqrt((x - center[0]) ** 2 + (y - center[1]) ** 2 + (z - center[2]) ** 2)
+            mask = (distance <= radius)
+            img_3d[mask] = value
 
-# take particles at t=0
-particles = extract_particles(f'{PATH_GTH}/VIRUS_snr_7_density_low.xml')
-particles_t0 = query_particles(particles, (lambda pa: True if pa.t == 0 else False))
+        # save slices
+        directory = f'{PATH_DTS_1}/segVirus_{snr.value}_{str(density.value)}'
+        if not (os.path.isdir(directory)): os.mkdir(directory)
+        for depth in range(10):
+            img = Image.fromarray(img_3d[:, :, depth])
+            img.save(f'{directory}/t_{str(time).zfill(3)}_z_{str(depth).zfill(2)}.tif')
+    return True
 
-# at this point: img_3d, detected_particles for img_3d
 
-# analyse
-# img_3d_t0 = img_3d.copy()
-img_3d_t0 = np.zeros_like(img_3d)
-x, y, z = np.meshgrid(np.arange(img_3d_t0.shape[0]), np.arange(img_3d_t0.shape[1]), np.arange(img_3d_t0.shape[2]))
+# plt.figure()
+# for i in range(4):
+#     plt.subplot(2,4,i+1)
+#     plt.title(f'image {i}')
+#     plt.imshow(Image.fromarray(img_3d[:, :, i]), cmap="gray")
+# for j in range(4):
+#     plt.subplot(2,4,j+4+1)
+#     plt.title(f'seg {j}')
+#     plt.imshow(Image.fromarray(img_3d_t0[:, :, j]), cmap="gray")
+# plt.savefig("comparison.png")
 
-# for p in particles_t0:
-#     x, y, z = round(p.x), round(p.y), np.clip(round(p.z), 0, 9)
-#     img_3d_t0[x, y, z] = 255
-#     img_3d_t0[:,:,z] = cv2.circle(img_3d_t0[:,:,z].astype(np.uint8), center=(x,y), radius=4, color=(254,254,254), thickness=cv2.FILLED)
 
-radius = 2
-for p in particles_t0:
-    center = (round(p.x), round(p.y), np.clip(round(p.z), 0, 9))
-    distance = np.sqrt((x - center[0]) ** 2 + (y - center[1]) ** 2 + (z - center[2]) ** 2)
-    mask = (distance <= radius)
-    img_3d_t0[mask] = 254
-    img_3d_t0[center[0], center[1], center[2]] = 255
-
-# comparison
-plt.figure()
-for i in range(4):
-    plt.subplot(2,4,i+1)
-    plt.title(f'image {i}')
-    plt.imshow(Image.fromarray(img_3d[:, :, i]), cmap="gray")
-for j in range(4):
-    plt.subplot(2,4,j+4+1)
-    plt.title(f'seg {j}')
-    plt.imshow(Image.fromarray(img_3d_t0[:, :, j]), cmap="gray")
-plt.savefig("comparison.png")
-
-print(f'num_particles_t0 = {len(particles_t0)}, num_elements = {np.count_nonzero(img_3d_t0 == 255)}')
-# FIX!! non li carica in ordine, forse
+if __name__ == '__main__':
+    make_segmaps_with_spheres(SNR.TYPE_7, Density.LOW, value=255)
