5f6cdcfd52e33452b38fa540741154371f0cb3c1
AlessiaChi5 <alessiachi93@hotmail.it>
2023-05-08 13:02:09 +0200

    fix: changed the name of path Dataset

--- utils/definitions.py
+++ utils/definitions.py
@@ -8,9 +8,9 @@ ROOT_DIR = Path(__file__).absolute().parent.parent
 DTS_DIR = os.path.join(ROOT_DIR, "Dataset")
 
 # Original Dataset
-DTS_CHALLANGE = os.path.join(DTS_DIR, "Challenge")
-DTS_VIRUS = os.path.join(DTS_CHALLANGE, "VIRUS")
-DTS_GTH = os.path.join(DTS_CHALLANGE, "ground_truth")
+DTS_CHALLENGE = os.path.join(DTS_DIR, "Challenge")
+DTS_VIRUS = os.path.join(DTS_CHALLENGE, "VIRUS")
+DTS_GTH = os.path.join(DTS_CHALLENGE, "ground_truth")
 
 # Segmented Datasets
 DTS_SEG_1 = os.path.join(DTS_DIR, "seg_technique_1")
