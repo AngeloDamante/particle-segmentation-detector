b9778627e1e3c664a20bbc0ba4ff3756929810b4
AngeloDamante <angelo.damante16@gmail.com>
2023-05-15 17:13:32 +0200

    fix: default value for segmenter class

--- ut/ut_segmenter.py
+++ ut/ut_segmenter.py
@@ -7,12 +7,12 @@ class TestSegmenter(unittest.TestCase):
     def test_segmenter_class(self):
         o_seg = Segmenter()
         self.assertEqual(o_seg.size, (512, 512, 10))
-        self.assertEqual((o_seg.sigma, o_seg.kernel, o_seg.radius, o_seg.value), (1.0, 3, 1, 255))
+        self.assertEqual((o_seg.sigma, o_seg.kernel, o_seg.radius, o_seg.value), (1.0, 5, 1, 255))
 
         o_seg.update_size((512, 512, 20))
         o_seg.update_values(sigma=0.8)
         self.assertEqual(o_seg.size, (512, 512, 20))
-        self.assertEqual((o_seg.sigma, o_seg.kernel, o_seg.radius, o_seg.value), (0.8, 3, 1, 255))
+        self.assertEqual((o_seg.sigma, o_seg.kernel, o_seg.radius, o_seg.value), (0.8, 5, 1, 255))
 
     def test_sphere_dts_without_slice(self):
         o_seg = Segmenter()
