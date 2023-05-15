245a75ba4d582a9a7f646d04c7b7545d262e3a8b
AngeloDamante <angelo.damante16@gmail.com>
2023-05-15 16:59:20 +0200

    feat: add compute_path libs to extract paths for segmentation dataset

--- utils/compute_path.py
+++ utils/compute_path.py
@@ -1,8 +1,9 @@
 import os
-from utils.Types import SNR, Density
-from utils.definitions import DTS_VIRUS, DTS_GTH
+from utils.Types import SNR, Density, SegMode
+from utils.definitions import DTS_VIRUS, DTS_GTH, DTS_DIR
 
-def get_img_path(snr: SNR, density: Density, t: int, depth: int) -> str:
+
+def get_slice_path(snr: SNR, density: Density, t: int, depth: int) -> str:
     """Compute slice path for identifying tuple(snr, density, t, depth)
 
     :param snr:
@@ -16,7 +17,7 @@ def get_img_path(snr: SNR, density: Density, t: int, depth: int) -> str:
     return path_file_img
 
 
-def get_gth_path(snr: SNR, density: Density) -> str:
+def get_gth_xml_path(snr: SNR, density: Density) -> str:
     """Compute path for file.xml with gth
 
     :param snr:
@@ -26,3 +27,31 @@ def get_gth_path(snr: SNR, density: Density) -> str:
     name = f'VIRUS_{snr.value}_{density.value}'
     path_file_gth = os.path.join(DTS_GTH, f'{name}.xml')
     return path_file_gth
+
+
+def get_seg_data_path(mode: SegMode, snr: SNR, density: Density, t: int) -> str:
+    """Compute path for npy data
+
+    :param snr:
+    :param density:
+    :param t:
+    :param mode:
+    :return:
+    """
+    dir_seg = f'segmentation_{mode.value}'
+    data_path = os.path.join(DTS_DIR, dir_seg, f'{snr.value}_{density.value}_data', f't_{str(t).zfill(3)}.npy')
+    return data_path
+
+def get_seg_slice_path(mode: SegMode, snr: SNR, density: Density, t: int, z: int) -> str:
+    """Compute segmented Slice path
+
+    :param mode:
+    :param snr:
+    :param density:
+    :param t:
+    :param z:
+    :return:
+    """
+    dir_seg = f'segmentation_{mode.value}'
+    slice_path = os.path.join(DTS_DIR, dir_seg, f'{snr.value}_{density.value}_img', f't_{str(t).zfill(3)}_z_{str(z).zfill(2)}.tiff')
+    return slice_path
