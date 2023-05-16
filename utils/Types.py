a806b0ae287ba06829bcf62db3f5d61b8e74d168
AngeloDamante <angelo.damante16@gmail.com>
2023-05-17 00:02:41 +0200

    feat: add dict to allow parsing values

--- utils/Types.py
+++ utils/Types.py
@@ -1,8 +1,10 @@
 from enum import Enum
 
+
 class SegMode(Enum):
     sphere = 'sphere'
     gauss = 'gauss'
+
 
 class SNR(Enum):
     TYPE_1 = 'snr_1'
@@ -10,7 +12,14 @@ class SNR(Enum):
     TYPE_4 = 'snr_4'
     TYPE_7 = 'snr_7'
 
+
 class Density(Enum):
     HIGH = 'density_high'
     MID = 'density_mid'
     LOW = 'density_low'
+
+
+# dict to parsing
+mapSegMode = {'sphere': SegMode.sphere, 'gauss': SegMode.gauss}
+mapSNR = {'snr_1': SNR.TYPE_1, 'snr_2': SNR.TYPE_2, 'snr_4': SNR.TYPE_4, 'snr_7': SNR.TYPE_7}
+mapDensity = {'density_high': Density.HIGH, 'density_mid': Density.MID, 'density_low': Density.LOW}
