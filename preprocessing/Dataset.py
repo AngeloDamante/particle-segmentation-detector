40d3c86683f720355f6a9137d896ae78fe212621
AngeloDamante <angelo.damante16@gmail.com>
2023-05-18 13:10:15 +0200

    wip: started for Torch Dataset

---
+++ preprocessing/Dataset.py
@@ -0,0 +1,12 @@
+import torch
+from torch.utils.data import Dataset
+
+class VirusDataset(Dataset):
+    def __init__(self, path, transform):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, item):
+        pass
