90d96968d8e6183ca59ac1df76d65f8d4ebeb27c
AngeloDamante <angelo.damante16@gmail.com>
2023-05-18 13:09:43 +0200

    fix: num flag changed into time_interval

--- create_segmaps.py
+++ create_segmaps.py
@@ -13,7 +13,7 @@ def main():
     parser.add_argument("-M", "--mode", type=str, default='gauss', help="chose mode to make segmentated map", choices=mapSegMode.keys(), required=True)
     parser.add_argument("-SNR", "--snr", type=str, default='snr_7', help="chose signal to noise ratio", choices=mapSNR.keys())
     parser.add_argument("-D", "--density", type=str, default='density_low', help="chose density", choices=mapDensity.keys())
-    parser.add_argument("-N", "--num", type=int, default=TIME_INTERVAL, help="number of images")
+    parser.add_argument("-T", "--time_interval", type=int, default=TIME_INTERVAL, help="number of images")
     parser.add_argument("-IMG", "--save_img", type=bool, default=False, help="save image")
 
     # to set Segmenter
@@ -27,7 +27,7 @@ def main():
     mode = mapSegMode[args.mode]
     snr = mapSNR[args.snr]
     density = mapDensity[args.density]
-    num = args.num
+    time_interval = args.time_interval
     save_img = args.save_img
     sigma = args.sigma
     radius = args.radius
@@ -36,7 +36,7 @@ def main():
 
     # create and use segmenter
     o_seg = Segmenter(sigma, kernel, radius, value)
-    o_seg.create_dataset(mode, snr, density, num, save_img)
+    o_seg.create_dataset(mode, snr, density, time_interval, save_img)
 
 
 if __name__ == '__main__':
