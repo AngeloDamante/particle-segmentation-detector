5d41bf08d79867e1f92270acb7fd88589664da4a
AngeloDamante <angelo.damante16@gmail.com>
2023-05-25 20:30:44 +0200

    feat: add methods to compute path for npz data and original image

--- utils/compute_path.py
+++ utils/compute_path.py
@@ -1,8 +1,31 @@
-"""Define method to compute paths for extraction phases"""
+"""Define method to compute paths for extraction phases
+
+Dataset
+├── Challenge
+│   ├── ground_truth -> get_gth_xml_path()
+│   │   ├── file.xml
+│   │   └── ...
+│   ├── VIRUS
+│   │   ├── VIRUS_snr_1_density_high -> get_slice_path()
+│   │   └── ...
+│   └── VIRUS_npy
+│       ├── snr_1_density_high -> get_data_path()
+│       └── ...
+└── Segmaps
+│    ├── Data
+│    │   ├── snr_1_density_high -> get_seg_data_path()
+│    │   └── ...
+│    └── Images
+│        ├── snr_1_density_high -> get_seg_slice_path()
+│        └── ...
+└── DTS
+    ├── snr_1_density_high -> get_npz_data_path()
+    └── ...
+"""
 
 import os
 from utils.Types import SNR, Density
-from utils.definitions import DTS_VIRUS, DTS_GTH, DTS_DIR, DTS_SEG_DATA, DTS_SEG_IMG
+from utils.definitions import DTS_VIRUS, DTS_GTH, DTS_SEG_DATA, DTS_SEG_IMG, DTS_DATA, DTS_COMPLETE
 
 
 def get_slice_path(snr: SNR, density: Density, t: int, depth: int) -> str:
@@ -32,16 +55,16 @@ def get_gth_xml_path(snr: SNR, density: Density) -> str:
 
 
 def get_seg_data_path(snr: SNR, density: Density, t: int) -> str:
-    """Compute path for npy data
+    """Compute path for npy seg data
 
     :param snr:
     :param density:
     :param t:
-    :param mode:
     :return:
     """
-    data_path = os.path.join(DTS_SEG_DATA, f'{snr.value}_{density.value}', f't_{str(t).zfill(3)}.npy')
-    return data_path
+    seg_data_path = os.path.join(DTS_SEG_DATA, f'{snr.value}_{density.value}', f't_{str(t).zfill(3)}.npy')
+    return seg_data_path
+
 
 def get_seg_slice_path(snr: SNR, density: Density, t: int, z: int) -> str:
     """Compute segmented Slice path
@@ -52,5 +75,29 @@ def get_seg_slice_path(snr: SNR, density: Density, t: int, z: int) -> str:
     :param z:
     :return:
     """
-    slice_path = os.path.join(DTS_SEG_IMG, f'{snr.value}_{density.value}', f't_{str(t).zfill(3)}_z_{str(z).zfill(2)}.tiff')
+    slice_path = os.path.join(DTS_SEG_IMG, f'{snr.value}_{density.value}',
+                              f't_{str(t).zfill(3)}_z_{str(z).zfill(2)}.tiff')
     return slice_path
+
+
+def get_data_path(snr: SNR, density: Density, t: int) -> str:
+    """Compute path for npy data
+
+    :param snr:
+    :param density:
+    :param t:
+    :return:
+    """
+    data_path = os.path.join(DTS_DATA, f'{snr.value}_{density.value}', f't_{str(t).zfill(3)}.npy')
+    return data_path
+
+def get_npz_data_path(snr:SNR, density:Density, t: int) -> str:
+    """Compute npz data path
+
+    :param snr:
+    :param density:
+    :param t:
+    :return:
+    """
+    npz_data_path = os.path.join(DTS_COMPLETE, f'{snr.value}_{density.value}', f't_{str(t).zfill(3)}.npz')
+    return npz_data_path
