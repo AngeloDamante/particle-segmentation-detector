5106ff8d3fdfc6093c45ab68b2e76ff134ca7e58
AngeloDamante <angelo.damante16@gmail.com>
2023-05-25 11:49:37 +0200

    fix: Sphere mode has been deleted

--- preprocessing/Segmenter.py
+++ preprocessing/Segmenter.py
@@ -8,9 +8,9 @@ from scipy import signal
 from typing import List, Tuple
 from preprocessing.analyser import extract_particles, query_particles
 from preprocessing.Particle import Particle
-from utils.Types import SegMode, SNR, Density
+from utils.Types import SNR, Density
 from utils.compute_path import get_gth_xml_path
-from utils.definitions import DTS_DIR
+from utils.definitions import DTS_DIR, DTS_SEG_IMG, DTS_SEG_DATA
 
 C, H, W, D = 1, 512, 512, 10
 SIZE_VOL = (C, H, W, D)
@@ -19,17 +19,15 @@ MAX_DEPTH = 10
 
 
 class Segmenter:
-    def __init__(self, sigma: float = 1.0, kernel: int = 5, radius: int = 1, value: int = 255):
+    def __init__(self, sigma: float = 1.0, kernel: int = 5, value: int = 255):
         """ Constructor
 
         :param sigma:
         :param kernel:
-        :param radius:
         :param value:
         """
         self.sigma: float = sigma
         self.kernel: int = kernel
-        self.radius: int = radius
         self.value: int = value
         self.size = (H, W, D)
 
@@ -41,22 +39,19 @@ class Segmenter:
         """
         self.size = size
 
-    def update_values(self, sigma: float = None, kernel: int = None, radius: int = None, value: int = None):
+    def update_values(self, sigma: float = None, kernel: int = None, value: int = None):
         """Update main values for both techniques
 
         :param sigma:
         :param kernel:
-        :param radius:
         :param value:
         :return:
         """
         if sigma is not None: self.sigma = sigma
         if kernel is not None: self.kernel = kernel
-        if radius is not None: self.radius = radius
         if value is not None: self.value = value
 
     def create_dataset(self,
-                       mode: SegMode,
                        snr: SNR,
                        density: Density,
                        time_interval: int = FINAL_TIME,
@@ -65,7 +60,6 @@ class Segmenter:
 
         :param snr:
         :param density:
-        :param mode:
         :param time_interval:
         :param save_img:
         :return:
@@ -78,38 +72,18 @@ class Segmenter:
         dts_img = []
         dts_gth = []
 
-        # make dir
-        dir_name = os.path.join(DTS_DIR, f'segmentation_{str(mode.value)}')
-        os.makedirs(dir_name, exist_ok=True)
+        # make dirs
+        os.makedirs(DTS_SEG_IMG, exist_ok=True)
+        os.makedirs(DTS_SEG_DATA, exist_ok=True)
 
         for t in tqdm(range(time_interval)):
             particles_t = query_particles(particles, (lambda pa, time=t: True if pa.t == time else False))
             img_3d = np.zeros(shape=(self.size[0], self.size[1], self.size[2]))  # (C,H,W,D)
-
-            if mode.value == SegMode.sphere.value:
-                img_3d = self._make_sphere(img_3d, particles_t)
-            if mode.value == SegMode.gauss.value:
-                img_3d = self._make_gauss(img_3d, particles_t)
-
+            img_3d = self._make_gauss(img_3d, particles_t)
             dts_img.append(img_3d)
             dts_gth.append(particles_t)
-            self._save_data(img_3d, t, os.path.join(dir_name, f'{snr.value}_{str(density.value)}'), save_img)
+            self._save_data(img_3d, t, f'{snr.value}_{str(density.value)}', save_img)
         return dts_img, dts_gth
-
-    def _make_sphere(self, img_3d: np.ndarray, particles: List[Particle]) -> np.ndarray:
-        """ Make segmentation with white spheres centered in particles coord
-
-        :param img_3d:
-        :param particles:
-        :return:
-        """
-        x, y, z = np.meshgrid(np.arange(img_3d.shape[0]), np.arange(img_3d.shape[1]), np.arange(img_3d.shape[2]))
-        for p in particles:
-            center = (round(p.x), round(p.y), np.clip(round(p.z), 0, MAX_DEPTH - 1))
-            distance = np.sqrt((x - center[0]) ** 2 + (y - center[1]) ** 2 + (z - center[2]) ** 2)
-            mask = (distance <= self.radius)
-            img_3d[mask] = self.value
-        return img_3d
 
     def _make_gauss(self, img_3d: np.ndarray, particles: List[Particle]) -> np.ndarray:
         """Make segmentation with convolution using gaussian filter
@@ -141,7 +115,7 @@ class Segmenter:
         :return:
         """
         # create dir data
-        dir_data = f'{directory}_data'
+        dir_data = os.path.join(DTS_SEG_DATA, directory)
         os.makedirs(dir_data, exist_ok=True)
 
         # save npy
@@ -149,7 +123,7 @@ class Segmenter:
 
         # create img dir
         if not save_img: return
-        dir_img = f'{directory}_img'
+        dir_img = os.path.join(DTS_SEG_IMG, directory)
         os.makedirs(dir_img, exist_ok=True)
 
         # save slices
