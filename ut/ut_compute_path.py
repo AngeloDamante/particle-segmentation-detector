0bf80001160d446868c8e5ed6af2cacfb7ca8d82
AngeloDamante <angelo.damante16@gmail.com>
2023-05-27 15:13:19 +0200

    test: ut for new policy

---
+++ ut/ut_compute_path.py
@@ -0,0 +1,33 @@
+import unittest
+import os
+from utils.compute_path import compute_name, get_data_path, get_slice_path, get_gth_xml_path
+from utils.definitions import DEFAULT_DATA_PATH, DEFAULT_SLICES_PATH, DEFAULT_GTH_PATH, DTS_RAW_PATH
+from utils.Types import SNR, Density
+
+
+class ComputePathTest(unittest.TestCase):
+    def test_compute_name(self):
+        self.assertEqual(compute_name(SNR.TYPE_7, Density.LOW), "snr_7_density_low")
+        self.assertEqual(compute_name(SNR.TYPE_7, Density.LOW, t=1), "snr_7_density_low_t001")
+        self.assertEqual(compute_name(SNR.TYPE_7, Density.LOW, t=1, z=4), "snr_7_density_low_t001_z04")
+        self.assertEqual(compute_name(SNR.TYPE_7, Density.LOW, z=3), "snr_7_density_low_z03")
+
+    def test_slice_path(self):
+        path = get_slice_path(SNR.TYPE_7, Density.LOW, t=0, z=0)
+        self.assertEqual(path, os.path.join(DEFAULT_SLICES_PATH, "snr_7_density_low", "snr_7_density_low_t000_z00.tif"))
+        path = get_slice_path(SNR.TYPE_7, Density.LOW, t=1, z=5, root=DEFAULT_SLICES_PATH)
+        self.assertEqual(path, os.path.join(DEFAULT_SLICES_PATH, "snr_7_density_low", "snr_7_density_low_t001_z05.tif"))
+
+    def test_data_path(self):
+        path = get_data_path(SNR.TYPE_7, Density.LOW, t=1)
+        self.assertEqual(path, os.path.join(DEFAULT_DATA_PATH, "snr_7_density_low", "snr_7_density_low_t001.npy"))
+        path = get_data_path(SNR.TYPE_7, Density.LOW, t=1, is_raw=True, root=DTS_RAW_PATH)
+        self.assertEqual(path, os.path.join(DTS_RAW_PATH, "snr_7_density_low", "snr_7_density_low_t001.npz"))
+
+    def test_gth_path(self):
+        path = get_gth_xml_path(SNR.TYPE_7, Density.LOW)
+        self.assertEqual(path, os.path.join(DEFAULT_GTH_PATH, "snr_7_density_low.xml"))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ ut/ut_segmenter_creation.py
@@ -0,0 +1,33 @@
+import unittest
+import numpy as np
+from preprocessing.segmentation import segment_data, gauss_conv
+from preprocessing.creation import slices_to_npy, make_raw_data
+from utils.Types import SNR, Density, Particle
+from utils.definitions import SIZE, DEFAULT_SLICES_PATH, DTS_RAW_PATH, DEFAULT_DATA_PATH
+
+
+class TestSegmenter(unittest.TestCase):
+    def test_gauss_conv(self):
+        img_3d = np.zeros(shape=(3, 3, 3))  # (H,W,D)
+        particles = [Particle(0, 1.0, 2.0, 2.0), Particle(2, 1.5, 0.8, 0.2)]
+        filtered_img = gauss_conv(img_3d, particles, kernel=3, sigma=0.5)
+        self.assertTrue(filtered_img is not None)
+        self.assertEqual(filtered_img.shape, (3, 3, 3))
+
+    def test_segment_data(self):
+        img_3d, particles = segment_data(SNR.TYPE_7, Density.LOW, t=5, kernel=3, sigma=0.5)
+        self.assertTrue(img_3d is not None)
+        self.assertTrue(len(particles) > 0)
+        self.assertEqual(img_3d.shape, (SIZE[0], SIZE[1], SIZE[2]))
+
+
+class TestCreation(unittest.TestCase):
+    def test_slices_to_npy(self):
+        slices_to_npy(SNR.TYPE_7, Density.LOW, DEFAULT_SLICES_PATH, DEFAULT_DATA_PATH)
+
+    def test_make_raw_data(self):
+        make_raw_data(SNR.TYPE_7, Density.LOW, kernel=3, sigma=0.5, dest_dir=DTS_RAW_PATH)
+
+
+if __name__ == '__main__':
+    unittest.main()
