b7adf174b43da1f0b8aa9dd14579d0894aff6900
AngeloDamante <angelo.damante16@gmail.com>
2023-06-01 13:13:22 +0200

    fix: fixed iteration for VirusDataset

--- techniques/unet/train.py
+++ techniques/unet/train.py
@@ -1,21 +1,19 @@
 """Implementation of UNET Training"""
+import os
+import datetime
 import torch.cuda
 import torch.nn as nn
 import torch.optim as optim
 from tqdm import tqdm
 from techniques.unet.model import UNET
 from techniques.unet.net_utils import (
-    load_checkpoint,
-    save_checkpoint,
     get_loaders,
     check_accuracy,
-    save_predictions_as_imgs
 )
 
 from utils.definitions import (
     DTS_TRAIN_PATH,
     DTS_VALIDATION_PATH,
-    DTS_TEST_PATH,
     DTS_ANALYZE_PATH,
     DEPTH,
     LEARNING_RATE,
@@ -23,6 +21,8 @@ from utils.definitions import (
     BATCH_SIZE,
     NUM_EPOCHS,
     NUM_WORKERS,
+    UNET_PATH,
+    UNET_RESULTS_PATH,
     IMG_HEIGHT,
     IMG_WIDTH,
     PIN_MEMORY,
@@ -31,13 +31,26 @@ from utils.definitions import (
 
 from preprocessing.Dataset import T
 
+CHECKPOINT_NAME = "checkpoint.pth.tar"
+
 
 def train_fn(loader, model, optimizer, loss_fn, scaler):
-    loop = tqdm(loader)
+    """Function for training
 
-    for batch_idx, (data, targets) in enumerate(loop):
-        data = data.to(device=DEVICE)
-        targets = targets.float().unsqueeze(1).to(device=DEVICE)
+    :param loader:
+    :param model:
+    :param optimizer:
+    :param loss_fn:
+    :param scaler:
+    :return:
+    """
+    loop = tqdm(loader)
+    for batch_idx, item in enumerate(loop):
+        # data = data.to(device=DEVICE)
+        # targets = targets.float().unsqueeze(1).to(device=DEVICE)
+
+        data = item['img'].to(device=DEVICE)
+        targets = item['target'].float().to(device=DEVICE)
 
         # forward
         with torch.cuda.amp.autocast():
@@ -55,10 +68,13 @@ def train_fn(loader, model, optimizer, loss_fn, scaler):
 
 
 def main():
+    # define all elements for training
     model = UNET(in_channels=DEPTH, out_channels=DEPTH).to(DEVICE)
     loss_fn = nn.CrossEntropyLoss()  # not binary
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    scaler = torch.cuda.amp.GradScaler()
 
+    # import loader for train and validation
     train_loader, val_loader = get_loaders(
         train_path=DTS_TRAIN_PATH,
         val_path=DTS_VALIDATION_PATH,
@@ -66,28 +82,32 @@ def main():
         train_transforms=T,
         val_transforms=T,
         num_workers=NUM_WORKERS,
-        pin_memory=PIN_MEMORY
+        pin_memory=PIN_MEMORY  # to improve speedup to transfer data from cpu to gpu
     )
 
+    # define train dir
+    train_name = datetime.datetime.today().strftime('day_%d_%m_%Y_time_%H_%M_%S')
+    dir_train = os.path.join(UNET_RESULTS_PATH, train_name)
+    os.makedirs(dir_train)
+
     if LOAD_MODEL:
-        load_checkpoint(torch.load("my_checkpoint.pth.tar"), model)  # TODO filename
-    check_accuracy(val_loader, model, device=DEVICE)
-    scaler = torch.cuda.amp.GradScaler()
+        print("=> Loading checkpoint")
+        checkpoint = torch.load(os.path.join(UNET_PATH, CHECKPOINT_NAME))
+        model.load_state_dict(checkpoint["state_dict"])
+        check_accuracy(val_loader, model, device=DEVICE)  # TODO
+        # load_checkpoint(torch.load("my_checkpoint.pth.tar"), model)
+
+    # start training
     for _ in range(NUM_EPOCHS):
         train_fn(train_loader, model, optimizer, loss_fn, scaler)
 
         # save model
-        checkpoint = {
-            "state_dict": model.state_dict(),
-            "optimizer": optimizer.state_dict(),
-        }
-        save_checkpoint(checkpoint)
+        checkpoint = {"state_dict": model.state_dict(), "optimizer": optimizer.state_dict()}
+        print("=> Saving checkpoint")
+        torch.save(checkpoint, CHECKPOINT_NAME)
 
         # check accuracy
         check_accuracy(val_loader, model, device=DEVICE)
-
-        # print some examples to a folder
-        save_predictions_as_imgs(val_loader, model, folder=DTS_ANALYZE_PATH, device=DEVICE)
 
 
 if __name__ == '__main__':
