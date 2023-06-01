d556fd076722cc7d921d2b7686825397eebe02f2
AngeloDamante <angelo.damante16@gmail.com>
2023-06-01 13:13:59 +0200

    fix: fixed iteration for VirusDataset and remove useless functions

--- techniques/unet/net_utils.py
+++ techniques/unet/net_utils.py
@@ -9,18 +9,18 @@ from utils.definitions import (
 )
 
 
-# TODO filename
-def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
-    print("=> Saving checkpoint")
-    torch.save(state, filename)
-
-
-def load_checkpoint(checkpoint, model):
-    print("=> Loading checkpoint")
-    model.load_state_dict(checkpoint["state_dict"])
-
-
 def get_loaders(train_path, val_path, batch_size, train_transforms, val_transforms, num_workers, pin_memory):
+    """Get Loaders
+
+    :param train_path:
+    :param val_path:
+    :param batch_size:
+    :param train_transforms:
+    :param val_transforms:
+    :param num_workers:
+    :param pin_memory:
+    :return:
+    """
     # training
     train_dts = VirusDataset(train_path, train_transforms)
     train_dtl = DataLoader(train_dts,
@@ -40,7 +40,24 @@ def get_loaders(train_path, val_path, batch_size, train_transforms, val_transfor
     return train_dtl, val_dtl
 
 
+# def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
+#     print("=> Saving checkpoint")
+#     torch.save(state, filename)
+
+#
+# def load_checkpoint(checkpoint, model):
+#     print("=> Loading checkpoint")
+#     model.load_state_dict(checkpoint["state_dict"])
+
+
 def check_accuracy(loader, model, device="cuda"):
+    """Check Accuracy
+
+    :param loader:
+    :param model:
+    :param device:
+    :return:
+    """
     num_correct = 0
     num_pixels = 0
     dice_score = 0
@@ -49,14 +66,16 @@ def check_accuracy(loader, model, device="cuda"):
     model.eval()
 
     with torch.no_grad():
-        for x, y in loader:
-            x = x.to(device)
-            y = y.to(device).unsqueeze(1)  # TODO
+        for data in loader:
+            x = data['img'].to(device)
+            y = data['target'].to(device)
+            # x = x.to(device)
+            # y = y.to(device).unsqueeze(1)
 
             preds = torch.sigmoid(model(x))
             preds = (preds > 0.5).float()
 
-            num_correct += (preds == y).sum()  # TODO
+            num_correct += (preds == y.round()).sum()  # TODO
             num_pixels += torch.numel(preds)
 
             # IoU equivalent for segmentation
@@ -66,18 +85,4 @@ def check_accuracy(loader, model, device="cuda"):
     print(f"Dice score: {dice_score / len(loader)}")
 
     # switch to train mode
-    model.train()
-
-# TODO folder name
-def save_predictions_as_imgs(loader, model, folder="saved_images/", device="cuda"):
-    model.eval()
-
-    for idx, (x, y) in enumerate(loader):
-        x = x.to(device=device)
-        with torch.no_grad():
-            preds = torch.sigmoid(model(x))
-            preds = (preds > 0.5).float()
-        torchvision.utils.save_image(preds, f"{folder}/pred_{idx}.png")
-        torchvision.utils.save_image(y.unsqueeze(1), f"{folder}{idx}.png")
-
     model.train()
