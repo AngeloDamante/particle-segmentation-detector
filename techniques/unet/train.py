216df162e2ef50da624c6cd356b2a36e6f31535d
AngeloDamante <angelo.damante16@gmail.com>
2023-06-05 02:10:03 +0200

    wip: add TODO to improve tqdm and handle load_model

--- techniques/unet/train.py
+++ techniques/unet/train.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 from techniques.unet.model import UNET
 from techniques.unet.net_utils import (
     get_loaders,
-    check_accuracy,
+    val_fn,
 )
 
 from utils.definitions import (
@@ -32,7 +32,7 @@ from utils.definitions import (
 
 from preprocessing.Dataset import train_transform, val_transform
 
-# CHECKPOINT_NAME = "checkpoint.pth.tar"
+CHECKPOINT_NAME = "checkpoint.pth.tar"
 
 
 def train_fn(loader, model, optimizer, loss_fn, scaler):
@@ -45,11 +45,8 @@ def train_fn(loader, model, optimizer, loss_fn, scaler):
     :param scaler:
     :return:
     """
-    loop = tqdm(loader)
-    for batch_idx, item in enumerate(loop):
-        # data = data.to(device=DEVICE)
-        # targets = targets.float().unsqueeze(1).to(device=DEVICE)
-
+    pbar = tqdm(enumerate(loader), total=len(loader))
+    for batch_idx, item in pbar:
         data = item['img'].to(device=DEVICE)
         targets = item['target'].float().to(device=DEVICE)
 
@@ -66,10 +63,10 @@ def train_fn(loader, model, optimizer, loss_fn, scaler):
         scaler.update()
 
         # update tqdm loop
-        loop.set_postfix()
+        pbar.set_description(f"Processing {batch_idx}/{len(loader)}, train_loss = {loss.item()}")
 
 
-def main(checkpoint_file: str = None):
+def main():
     # define all elements for training
     model = UNET(in_channels=DEPTH, out_channels=DEPTH).to(DEVICE)
     # loss_fn = nn.CrossEntropyLoss()
@@ -90,14 +87,13 @@ def main(checkpoint_file: str = None):
 
     # define train dir
     train_name = datetime.datetime.today().strftime('day_%d_%m_%Y_time_%H_%M_%S')
-    os.makedirs(UNET_RESULTS_CHECKPOINTS, exist_ok=True)
+    os.makedirs(UNET_RESULTS_CHECKPOINTS, exist_ok=True)  # FIXME useless
 
-    if checkpoint_file is not None:
+    if LOAD_MODEL:
         print("=> Loading checkpoint")
-        checkpoint = torch.load(checkpoint_file)
+        checkpoint = torch.load(CHECKPOINT_NAME)
         model.load_state_dict(checkpoint["state_dict"])
-        check_accuracy(val_loader, model, device=DEVICE)  # TODO
-        # load_checkpoint(torch.load("my_checkpoint.pth.tar"), model)
+        val_fn(val_loader, model, loss_fn, device=DEVICE)
 
     # start training
     for _ in range(NUM_EPOCHS):
@@ -109,7 +105,10 @@ def main(checkpoint_file: str = None):
         torch.save(checkpoint, os.path.join(UNET_RESULTS_CHECKPOINTS, f'{train_name}.pth.tar'))
 
         # check accuracy
-        check_accuracy(val_loader, model, device=DEVICE)
+        val_fn(val_loader, model, loss_fn, device=DEVICE)
+
+        # save
+        # TODO save slices!
 
 
 if __name__ == '__main__':
