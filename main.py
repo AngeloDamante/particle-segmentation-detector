b8cbea1d8f3d5b4d32b96ef59ebe6a2fac62ca93
AngeloDamante <angelo.damante16@gmail.com>
2023-06-06 20:44:06 +0200

    chore: add print device

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ print(cv2.__version__)
 print(torch.__version__)
 print(torch.cuda.is_available())
 device = "cuda" if torch.cuda.is_available() else "cpu"
+num_device = torch.cuda.device_count()
 print(device)
+print(num_device)
