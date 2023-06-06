e59b414f4153af04677e4f165981c6a176b9ffd2
AngeloDamante <angelo.damante16@gmail.com>
2023-06-06 20:23:05 +0200

    feat: add functions to save slices in comparison mode

--- preprocessing/analyser.py
+++ preprocessing/analyser.py
@@ -58,15 +58,45 @@ def draw_particles(img_3d: np.ndarray, particles: List[Particle]) -> np.ndarray:
     return img
 
 
-def comparison(x: np.ndarray, y: np.ndarray, save_dir: str):
+def comparison_pred(x: np.ndarray, y: np.ndarray, y_hat: np.ndarray, save_dir: str):
+    """Comparison viewer for image, target and prediction
+
+    :param x:
+    :param y:
+    :param y_hat:
+    :param save_dir:
+    :return:
+    """
+    os.makedirs(save_dir, exist_ok=True)
+    for z in range(x.shape[2]):
+        cmp_img = np.hstack((x[:, :, z], y[:, :, z], y_hat[:, :, z]))
+        cv2.imwrite(os.path.join(save_dir, f'{str(z).zfill(3)}.png'), cmp_img)  # FIXME
+
+def comparison_seg(x: np.ndarray, y: np.ndarray, save_dir: str):
     """Comparison viewer between x and y
 
     :param x:
     :param y:
+    :param y_hat:
     :param save_dir:
     :return:
     """
     os.makedirs(save_dir, exist_ok=True)
     for z in range(x.shape[2]):
         cmp_img = np.hstack((x[:, :, z], y[:, :, z]))
-        cv2.imwrite(os.path.join(save_dir, f'{str(z).zfill(3)}.png'), cmp_img)
+        cv2.imwrite(os.path.join(save_dir, f'{str(z).zfill(3)}.png'), cmp_img)  # FIXME
+
+
+def save_slices(img_3d: np.ndarray, save_dir: str, img_name: str = None):
+    """Save slices of input 3d image
+
+    :param img_3d: H,W,D
+    :param save_dir:
+    :param img_name:
+    :return:
+    """
+    if img_name is None: img_name = "img"
+    depth = img_3d.shape[2]
+    os.makedirs(save_dir, exist_ok=True)
+    for z in range(depth):
+        cv2.imwrite(os.path.join(save_dir, f'{img_name}_{str(z)}.png'), img_3d[:, :, z])
