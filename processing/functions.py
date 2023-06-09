2e62a1774551d6ed1a4ae6f5e468dedcff69e572
AngeloDamante <angelo.damante16@gmail.com>
2023-06-09 12:50:09 +0200

    fix: inference fixed to handle exchange tensors between gpu and cpu

--- processing/functions.py
+++ processing/functions.py
@@ -39,7 +39,7 @@ def train_fn(loader, model, optimizer, loss_fn, scaler):
         scaler.update()
 
         # update tqdm loop
-        pbar.set_description(f"Processing {batch_idx + 1}/{len(loader)}, train_loss = {loss.item()}")
+        pbar.set_description(f"Processing {batch_idx + 1}/{len(loader)}, train_loss = {loss.item():.5f}")
 
 
 def val_fn(loader, model, loss, device="cuda") -> float:
@@ -70,8 +70,8 @@ def val_fn(loader, model, loss, device="cuda") -> float:
             # TODO serve qualcosa per verificare che le particelle trovate siano quelle della gth
             dice_score += (2 * (preds * y).sum()) / ((preds + y).sum() + 1e-8)
 
-        print(f'val loss: {val_loss / len(loader)}')
-        print(f"Dice score: {dice_score / len(loader)}")
+        print(f'val loss: {(val_loss / len(loader)):.5f}')
+        print(f"Dice score: {(dice_score / len(loader)):.5f}")
 
     # switch to train mode
     model.train()
@@ -93,10 +93,11 @@ def inference(model: torch.nn.Module, snr: SNR, density: Density, t: int, save_d
     x_orig = x.copy()
     y = data['target']
     x = np.divide(x, 255.0, dtype=np.float32)
-    x = torch.from_numpy(x)
+    x = torch.from_numpy(x).to(DEVICE)
     x = torch.permute(x, (2, 0, 1)).unsqueeze(0)
 
     with torch.no_grad():
-        preds = torch.sigmoid(model(x)).squeeze(0)
-        y_hat = torch.permute(preds, (1, 2, 0)).numpy() * 255
+        preds = torch.sigmoid(model(x))
+        preds = preds.to('cpu')
+        y_hat = torch.permute(preds.squeeze(0), (1, 2, 0)).numpy() * 255
         comparison_pred(x_orig, y, y_hat, os.path.join(save_dir, f'{compute_name(snr, density, t)}'))
