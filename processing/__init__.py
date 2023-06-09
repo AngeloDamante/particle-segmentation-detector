5aaf3c486c9361ba68219fe1a7349a63cece751c
AngeloDamante <angelo.damante16@gmail.com>
2023-06-09 10:14:15 +0200

    docs: add comments to explain layout

---
+++ processing/__init__.py
@@ -0,0 +1,7 @@
+"""In this module, we define the functions to use in processing phase
+
+processing
+    ├── blob_detector.py        -> to detect blob in the images
+    ├── functions.py            -> define train and validation functions
+    └── utilities.py            -> utility functions for processing phase
+"""
