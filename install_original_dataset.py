6fc13a9c997a30645c6dd57566cc6ab82f934078
AngeloDamante <angelo.damante16@gmail.com>
2023-06-09 10:17:58 +0200

    feat: add script to install working dataset

--- install_original_dataset.py
+++ install_original_dataset.py
@@ -1,33 +1,38 @@
+"""Run this script to download and install original Dataset provided by Challenge"""
+
 import os
 import logging
-import platform
-from utils.definitions import mapSNR, mapDensity, DEFAULT_SLICES_PATH, DEFAULT_DATA_PATH, DTS_DIR
+import tarfile
 from preprocessing.creation import slices_to_npy
 from utils.logger import configure_logger
+from utils.definitions import (
+    mapSNR,
+    mapDensity,
+    DEFAULT_SLICES_PATH,
+    DEFAULT_DATA_PATH,
+    DTS_DIR
+)
+
+configure_logger(log_lvl=logging.INFO)
 
 ARCHIVE_NAME = 'Challenge_dts.tar.xz'
 ARCHIVE_PATH = os.path.join(DTS_DIR, ARCHIVE_NAME)
 ARCHIVE_LINK = 'https://github.com/AngeloDamante/particle-ViT-segmentation/releases/download/v1.0/Challenge_dts.tar.xz'
 
-configure_logger(logging.INFO)
-logging.info(f"This script is running on {platform.system()}")
-
-# install phase
+# installation phase
 logging.info("Install Original Dataset")
 os.system(f'curl -Lk {ARCHIVE_LINK} > {ARCHIVE_PATH}')
-os.system(f'tar -xJv -f {ARCHIVE_PATH} --directory {DTS_DIR}')
 
-# clean phase
-if platform.system() == 'Windows':
-    os.system(f'del {ARCHIVE_PATH}')
-else:
-    os.system(f'rm {ARCHIVE_PATH}')
+# extraction phase
+logging.info(f"Extracting Original Dataset in {DTS_DIR}")
+with tarfile.open(ARCHIVE_PATH, 'r') as f:
+    f.extractall(DTS_DIR, numeric_owner=True)
+os.remove(ARCHIVE_PATH)
 
-# make npy
-logging.info(f"Make Data from original dataset in {DEFAULT_DATA_PATH}")
+# make npy phase
+logging.info(f"Make Raw Data from original dataset in {DEFAULT_DATA_PATH}")
 for snr in mapSNR.values():
     for density in mapDensity.values():
         logging.info(f'processing: snr = {snr.value}, density = {density.value}')
         slices_to_npy(snr, density, DEFAULT_SLICES_PATH, DEFAULT_DATA_PATH)
-
 logging.info('[DONE]')
