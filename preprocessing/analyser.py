666eb39fadbd1995d27d1f8343a724ce8fbf1d90
AngeloDamante <angelo.damante16@gmail.com>
2023-06-09 19:57:16 +0200

    feat: improve comparison_seg adding divider

--- preprocessing/analyser.py
+++ preprocessing/analyser.py
@@ -79,14 +79,14 @@ def comparison_seg(x: np.ndarray, y: np.ndarray, save_dir: str):
 
     :param x:
     :param y:
-    :param y_hat:
     :param save_dir:
     :return:
     """
     os.makedirs(save_dir, exist_ok=True)
     for z in range(x.shape[2]):
-        cmp_img = np.hstack((x[:, :, z], y[:, :, z]))
-        cv2.imwrite(os.path.join(save_dir, f'{str(z).zfill(3)}.png'), cmp_img)  # FIXME
+        divider = np.ones((x.shape[0], 3)) * 255
+        cmp_img = np.hstack((x[:, :, z], divider, y[:, :, z]))
+        cv2.imwrite(os.path.join(save_dir, f'{str(z).zfill(3)}.png'), cmp_img.astype(np.uint8))  # FIXME
 
 
 def save_slices(img_3d: np.ndarray, save_dir: str, img_name: str = None):
