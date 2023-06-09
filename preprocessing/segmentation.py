00ff49877e9ba7d0fc64feeb77949bbe7bbd67d0
AngeloDamante <angelo.damante16@gmail.com>
2023-06-09 10:13:03 +0200

    chore: clean code

--- preprocessing/segmentation.py
+++ preprocessing/segmentation.py
@@ -1,15 +1,12 @@
 """Define functions to create segmentated dataset"""
 
 import numpy as np
-import cv2
 from scipy import signal
 from typing import List, Tuple
 from preprocessing.analyser import extract_particles, query_particles
 from utils.Types import SNR, Density, Particle
 from utils.compute_path import get_gth_xml_path
 from utils.definitions import SIZE, DEPTH
-
-WHITE_POINT = 255
 
 
 def segment_data(snr: SNR, density: Density, t: int, kernel: int, sigma: float) -> Tuple[np.ndarray, List[Particle]]:
@@ -27,10 +24,8 @@ def segment_data(snr: SNR, density: Density, t: int, kernel: int, sigma: float) 
     particles = extract_particles(path_gth)
     particles_t = query_particles(particles, (lambda pa, time=t: True if pa.t == time else False))
 
-    # potremmo fare 100 slice per ridurre l'errore l'asse z di un fattore 100 e poi interpolare le slice (basta un resize)
-
     # compute segmaps
-    img_3d = np.zeros(shape=(SIZE[0], SIZE[1], SIZE[2] * 10))  # (H,W,D)
+    img_3d = np.zeros(shape=(SIZE[0], SIZE[1], SIZE[2]))  # (H,W,D)
     img_3d = gauss_conv(img_3d, particles_t, kernel, sigma)
     return img_3d, particles_t
 
@@ -49,25 +44,12 @@ def gauss_conv(img_3d: np.ndarray, particles: List[Particle], kernel: int, sigma
     kernel = np.exp(-(x ** 2 + y ** 2 + z ** 2) / (2 * sigma ** 2))
 
     for p in particles:
-        # center = (round(p.x), round(p.y), np.clip(round(p.z), 0, DEPTH - 1))
-        center = (round(p.x), round(p.y), np.clip(round(p.z * 10), 0, DEPTH * 10 - 1))
-        img_3d[center] = WHITE_POINT
+        center = (round(p.x), round(p.y), np.clip(round(p.z), 0, DEPTH - 1))
+        img_3d[center] = 255  # white point
 
-    filtered_left = np.clip(signal.convolve(img_3d, kernel, mode="same"), 0, 255).astype(np.uint8)
-    filtered_mirror = np.rot90(filtered_left, k=1, axes=(0, 1))
+    filtered = signal.convolve(img_3d, kernel, mode="same")
+    filtered_mirror = np.rot90(filtered, k=1, axes=(0, 1))
     filtered = np.flipud(filtered_mirror)
-
-    # FIXME
-    from scipy.interpolate import interp1d
-    # ntime, nheight_in, nlat, nlon = (10, 20, 30, 40)
-    heights = np.linspace(0, 1, DEPTH * 10)
-    # t_in = np.random.normal(size=(ntime, nheight_in, nlat, nlon))
-    f_out = interp1d(heights, filtered, axis=2)
-    nheight_out = DEPTH
-    new_heights = np.linspace(0, 1, nheight_out)
-    filtered_interp = f_out(new_heights)
-
-    # import pdb
-    # pdb.set_trace()
-
-    return filtered_interp
+    filtered = np.resize(filtered, (512, 512, 10))
+    filtered = np.clip(filtered, 0, 255).astype(np.uint8)
+    return filtered
