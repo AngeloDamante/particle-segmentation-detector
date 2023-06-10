b1424d12a77c85af415e8e036cb2d45bdaf7f66a
AngeloDamante <angelo.damante16@gmail.com>
2023-06-11 01:44:36 +0200

    feat: add tensorboard writer to track train_loss, val_loss and dice_score

--- train_model.py
+++ train_model.py
@@ -4,18 +4,19 @@ import os
 import shutil
 import argparse
 import logging
+from tqdm import tqdm
 import torch.cuda
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
 from models.unet import UNET
 from models.vit import SegFormer
 from preprocessing.Dataset import train_transform, val_transform
 from processing.utilities import get_loaders, CheckpointSaver
-from processing.functions import train_fn, val_fn
+from processing.functions import train_fn, val_fn, inference_fn
 from utils.logger import configure_logger
 from utils.Types import SNR, Density
 from utils.compute_path import get_data_path
-from utils.json_parser import json_parser_train
 from utils.definitions import (
     TIME_INTERVAL,
     DTS_TRAIN_PATH,
@@ -27,8 +28,7 @@ from utils.definitions import (
     DEVICE,
     mapSNR,
     mapDensity,
-    RESULTS_UNET_CHECKPOINT,
-    RESULTS_VIT_CHECKPOINT
+    EXPERIMENTS_PATH
 )
 
 configure_logger(logging.INFO)
@@ -71,20 +71,41 @@ def split_dataset(snr: SNR, density: Density, p_train: int = 80, is_test=False):
         shutil.copy2(path, DTS_VALIDATION_PATH)
 
 
+def json_parser(path_file: str):
+    """Json parser to take settings for training
+
+    :param path_file:
+    :return:
+    """
+    if not os.path.exists(path_file): return {}, {}, {}
+    with open(path_file, 'r') as json_file:
+        json_data = json.load(json_file)
+        settings = json_data['settings']
+        training_dts = json_data['training_dts']
+        hyperparameters = json_data['hyperparameters']
+    return settings, training_dts, hyperparameters
+
+
 def main():
     # parsing
     parser = argparse.ArgumentParser()
-    # default_config_file = 'snr_7_density_low_train.json'
-    default_config_file = 'snr_7_density_low_train_vit.json'
-    parser.add_argument("-C", "--config", type=str, default=default_config_file, help=f"name of config in {CONFIG_DIR}")
+    default_config_file = os.path.join(CONFIG_DIR, 'train_unet.json')
+    parser.add_argument("-C", "--config", type=str, default=default_config_file, help="absolute path of config file")
     args = parser.parse_args()
-    config_file = os.path.join(CONFIG_DIR, args.config)
+    config_file = args.config
 
     # extracting settings
-    logging.info('[ EXTRACTING JSON SETTINGS ]')
+    logging.info('[ EXTRACTING JSON DATA ]')
     settings, training_dts, hyperparameters = json_parser_train(config_file)
     train_name = settings['training_name']
     p_split = settings['p_split']
+    experiment_dir = os.path.join(EXPERIMENTS_PATH, train_name)
+
+    # create writer for tensorboard
+    logging.info(f"[ EXPERIMENT {train_name} SAVING ON {experiment_dir} ]")
+    os.makedirs(experiment_dir, exist_ok=True)
+    writer = SummaryWriter(log_dir=experiment_dir, filename_suffix=train_name)
+    shutil.copy2(config_file, os.path.join(experiment_dir, f'{train_name}.json'))
 
     # make dataset for training
     logging.info('[ TRAINING DATASET CREATION ]')
@@ -94,20 +115,15 @@ def main():
     for dts in training_dts:
         snr = mapSNR[dts['snr']]
         density = mapDensity[dts['density']]
-        logging.info(f'[ TRAIN ]: snr = {snr.value},  density = {density.value}')
-        split_dataset(snr, density, p_split)
+        logging.info(f'[ DATASET: snr = {snr.value},  density = {density.value} ]')
+        split_dataset(snr, density, p_split, is_test=True)
 
     # chose model
     logging.info(f'[ CHOSEN MODEL = {settings["model"]} ]')
     model = nn.Module()
-    checkpoint_saver = CheckpointSaver()
     if settings['model'] == 'unet':
         model = UNET(in_channels=DEPTH, out_channels=DEPTH).to(DEVICE)
-        os.makedirs(RESULTS_UNET_CHECKPOINT, exist_ok=True)
-        checkpoint_saver.set_file_name(os.path.join(RESULTS_UNET_CHECKPOINT, f'{train_name}.pth.tar'))
     elif settings['model'] == 'vit':
-        os.makedirs(RESULTS_VIT_CHECKPOINT, exist_ok=True)
-        checkpoint_saver.set_file_name(os.path.join(RESULTS_VIT_CHECKPOINT, f'{train_name}.pth.tar'))
         model = SegFormer(
             in_channels=10,
             widths=[64, 128, 256, 512],
@@ -139,6 +155,7 @@ def main():
     )
 
     # load model only if is present
+    checkpoint_saver = CheckpointSaver(file_name=os.path.join(experiment_dir, f'{train_name}.pth.tar'))
     if hyperparameters['load_model'] and checkpoint_saver.is_present():
         logging.info(f'[ LOAD MODEL {checkpoint_saver.get_file_name()}]')
         print("=> Loading checkpoint")
@@ -147,18 +164,35 @@ def main():
         val_fn(val_loader, model, loss_fn, device=DEVICE)
 
     # start training
-    logging.info('[ TRAINING STARTED ]')
+    logging.info(f'[ TRAINING STARTED with {DEVICE} device ]')
     num_epochs = hyperparameters['num_epochs']
     for epoch in range(num_epochs):
         print(f'----------------------------------------> epoch = {epoch + 1}/{num_epochs}')
         # train
-        train_fn(train_loader, model, optimizer, loss_fn, scaler)
+        train_loss = train_fn(train_loader, model, optimizer, loss_fn, scaler)
+        writer.add_scalar("Loss/train", train_loss, epoch)
 
         # validation
-        val_loss = val_fn(val_loader, model, loss_fn, device=DEVICE)
+        val_loss, dice_score = val_fn(val_loader, model, loss_fn, device=DEVICE)
+        writer.add_scalar("Loss/val", val_loss, epoch)
+        writer.add_scalar("Dice_score", dice_score, epoch)
 
         # save model
         checkpoint_saver.save_checkpoint(val_loss, model.state_dict(), optimizer.state_dict())
+
+    # tensorboard
+    writer.close()
+    logging.info('[ TRAINING FINISHED ]')
+
+    # inference - testing
+    logging.info("[ TESTING TRAINED MODEL ]")
+    img_dir = os.path.join(experiment_dir, 'Images')
+    checkpoint = torch.load(checkpoint_saver.get_file_name())
+    model.load_state_dict(checkpoint["state_dict"])
+    for dts in training_dts:
+        for t in tqdm(range(settings['p_split'], TIME_INTERVAL)):
+            inference_fn(model, mapSNR[dts['snr']], mapDensity[dts['density']], t=t, save_dir=img_dir)
+    logging.info("[ DONE ]")
 
 
 if __name__ == '__main__':
